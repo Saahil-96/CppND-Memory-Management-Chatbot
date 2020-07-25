cff823735be73a706e7cc9da9bc6f45cb8993df4
Clarkkent96 <saahil96@gmail.com>
2020-07-25 01:15:27 -0400

    First Commit

---
+++ src/graphnode.cpp
@@ -0,0 +1,60 @@
+#include "graphedge.h"
+#include "graphnode.h"
+
+GraphNode::GraphNode(int id)
+{
+    _id = id;
+}
+
+GraphNode::~GraphNode()
+{
+    //// STUDENT CODE
+    ////
+    
+    // delete _chatBot; // Task 0
+
+    ////
+    //// EOF STUDENT CODE
+}
+
+void GraphNode::AddToken(std::string token)
+{
+    _answers.push_back(token);
+}
+
+void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
+{
+    _parentEdges.push_back(edge);
+}
+
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
+{
+    _childEdges.push_back(std::move(edge)); 
+}
+
+//// STUDENT CODE
+//// Task 5
+void GraphNode::MoveChatbotHere(ChatBot chatbot) 
+{
+    _chatBot = std::move(chatbot);
+    _chatBot.SetCurrentNode(this);
+}
+
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
+{
+    newNode->MoveChatbotHere(std::move(_chatBot)); //task 5
+    // _chatBot = nullptr; //task 5
+}
+////
+//// EOF STUDENT CODE
+
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
+{
+    //// STUDENT CODE
+    //// Task 4
+
+    return _childEdges[index].get();
+
+    ////
+    //// EOF STUDENT CODE
+}
